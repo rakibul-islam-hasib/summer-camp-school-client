879235d40aad9d234f50376213b37f298ab2fbb7
Hasib Hossain <boraborhasib@gmail.com>
2023-06-06 22:46:17 +0600

    Navbar And footer added

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <h1 className='text-2xl text-red-400 text-center'>Footer</h1>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/components/headers/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <div>
+            <h1 className='text-2xl text-red-400 text-center'>NavBar</h1>
+        </div>
+    );
+};
+
+export default NavBar;
