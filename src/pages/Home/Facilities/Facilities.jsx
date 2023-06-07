bd46e5c2ba11fa5fe6fafde67d2d4a90de10643a
Hasib Hossain <boraborhasib@gmail.com>
2023-06-07 11:20:11 +0600

    Facilites part ok

--- src/pages/Home/Facilities/Facilities.jsx
+++ src/pages/Home/Facilities/Facilities.jsx
@@ -7,8 +7,8 @@ const Facilities = () => {
     return (
         <div className='lg:w-[90%] w-[95%] mx-auto my-20'>
             <div className="md:flex gap-5">
-                <div className="h-[450px]">
-                    <img className='h-[450px] w-full' src={girlImg} alt="" />
+                <div className="h-[450px] md:h-full">
+                    <img className='h-[450px] md:h-full w-full' src={girlImg} alt="" />
                 </div>
                 <div className="px-7 py-6 shadow-xl">
                     <h1 className='text-xl ml-5 text-secondary font-bold'>TOP FACILITIES</h1>
@@ -38,11 +38,11 @@ const Facilities = () => {
                     </div>
                     <div className="mt-8 grid justify-items-center grid-cols-2">
                         <div className="text-center">
-                            <h1 className='text-5xl font-bold'><CountUp duration={0.4} end={1800} />+</h1>
+                            <h1 className='text-5xl font-bold'><CountUp duration={2} end={1800} />+</h1>
                             <p className='text-xl font-bold text-secondary mt-2'>Students</p>
                         </div>
                         <div className="">
-                            <h1 className='text-5xl font-bold'><CountUp duration={0.4} end={450} />+</h1>
+                            <h1 className='text-5xl font-bold'><CountUp duration={5} end={450} />+</h1>
                             <p className='text-xl font-bold text-secondary mt-2'>Instructors</p>
                         </div>
                     </div>
