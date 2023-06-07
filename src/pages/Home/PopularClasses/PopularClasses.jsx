31ea345d93357620d5b81c931f18670777ce4767
Hasib Hossain <boraborhasib@gmail.com>
2023-06-07 13:27:09 +0600

    Popular classes added

---
+++ src/pages/Home/PopularClasses/PopularClasses.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PopularClasses = () => {
+    return (
+        <div>
+            <div className="">
+                
+            </div>
+        </div>
+    );
+};
+
+export default PopularClasses;
