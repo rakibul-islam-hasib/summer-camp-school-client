2fc51f072c5fd99c77cf96ba3b4a7f8e3057e179
Hasib Hossain <boraborhasib@gmail.com>
2023-06-07 10:31:35 +0600

    Hero section updated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Hero from './Hero/Hero';
 import { useTitle } from '../../hooks/useTitle';
+import Facilities from './Facilities/Facilities';
 
 const Home = () => {
     useTitle('Home | Sound Safari');
     return (
         <section>
             <Hero /> 
-            <h1>Tdbjv  </h1> 
+            <Facilities />
         </section>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ export default {
   theme: {
     extend: {
       colors: {
-        'primary': '#FF1949',
+        'primary': '#b68bdcb1',
+        'secondary': '#FF1949',
       }
     },
   },
