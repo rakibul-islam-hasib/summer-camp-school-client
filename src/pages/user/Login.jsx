ca2c9dd3de26c2157d9ce49519b5a15e3c758d87
Hasib Hossain <boraborhasib@gmail.com>
2023-06-07 22:05:30 +0600

    Login page added

--- src/pages/user/Login.jsx
+++ src/pages/user/Login.jsx
@@ -1,41 +1,102 @@
 import React from 'react';
 import { useTitle } from '../../hooks/useTitle';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     useTitle('Login | Sound Safari');
     return (
-        <div className="flex items-center justify-center h-screen bg-gray-100">
-            <div className="bg-white p-8 rounded shadow-lg">
-                <h2 className="text-2xl font-semibold mb-4">Login</h2>
-                <form>
-                    <div className="mb-4">
-                        <label htmlFor="email" className="block text-gray-700 font-medium mb-1">
-                            Email Address
-                        </label>
-                        <input
-                            type="email"
-                            id="email"
-                            className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-indigo-500"
-                            placeholder="Enter your email"
-                        />
+        <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+            <div className="mx-auto max-w-lg">
+                <h1 className="text-center text-2xl font-bold text-secondary sm:text-3xl">
+                    Get started today
+                </h1>
+
+                <p className="mx-auto mt-4 max-w-md text-center text-gray-500">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati sunt
+                    dolores deleniti inventore quaerat mollitia?
+                </p>
+
+                <form
+                    action=""
+                    className="mb-0 mt-6 space-y-4 rounded-lg p-4 shadow-lg sm:p-6 lg:p-8"
+                >
+                    <p className="text-center text-lg font-medium">Sign in to your account</p>
+
+                    <div>
+                        <label for="email" className="sr-only">Email</label>
+
+                        <div className="relative">
+                            <input
+                                type="email"
+                                className="w-full border outline-none rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
+                                placeholder="Enter email"
+                            />
+
+                            <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-4 w-4 text-gray-400"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+                                    />
+                                </svg>
+                            </span>
+                        </div>
                     </div>
-                    <div className="mb-6">
-                        <label htmlFor="password" className="block text-gray-700 font-medium mb-1">
-                            Password
-                        </label>
-                        <input
-                            type="password"
-                            id="password"
-                            className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-indigo-500"
-                            placeholder="Enter your password"
-                        />
+                {/* Password Field */}
+                    <div>
+                        <label for="password" className="sr-only">Password</label>
+
+                        <div className="relative">
+                            <input
+                                type="password"
+                                className="w-full rounded-lg outline-none border border-gray-200 p-4 pe-12 text-sm shadow-sm"
+                                placeholder="Enter password"
+                            />
+
+                            <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-4 w-4 text-gray-400"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                >
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        stroke-width="2"
+                                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                                    />
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        stroke-width="2"
+                                        d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                                    />
+                                </svg>
+                            </span>
+                        </div>
                     </div>
+
                     <button
                         type="submit"
-                        className="bg-indigo-500 text-white py-2 px-4 rounded hover:bg-indigo-600"
+                        className="block w-full rounded-lg bg-secondary px-5 py-3 text-sm font-medium text-white"
                     >
-                        Login
+                        Sign in
                     </button>
+
+                    <p className="text-center text-sm text-gray-500">
+                        No account?
+                        <Link className="underline" to='/register'>Sign up</Link>
+                    </p>
                 </form>
             </div>
         </div>
