51f9f8a385301f9192fae49bc9326cdfcd02bdf8
Hasib Hossain <boraborhasib@gmail.com>
2023-06-08 14:48:59 +0600

    Manage user ok

--- src/pages/Dashboard/Admin/users/ManageUsers.jsx
+++ src/pages/Dashboard/Admin/users/ManageUsers.jsx
@@ -1,9 +1,68 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import useAxiosFetch from '../../../../hooks/useAxiosFetch';
+import { GrUpdate } from 'react-icons/gr'
+import { FcDeleteDatabase } from 'react-icons/fc';
 const ManageUsers = () => {
+
+    const axiosFetch = useAxiosFetch();
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        axiosFetch.get('/users')
+            .then(res => {
+                setUsers(res.data)
+            })
+            .catch(err => console.log(err))
+    }, [])
+
+
     return (
         <div>
-            <h1>Manage Users</h1>
+            <h1 className='text-center text-4xl font-bold my-7'>Manage <span className='text-secondary'>Users</span></h1>
+
+            <div className="">
+
+                <div className="flex flex-col">
+                    <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                        <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+                            <div className="overflow-hidden">
+                                <table className="min-w-full text-left text-sm font-light">
+                                    <thead className="border-b font-medium dark:border-neutral-500">
+                                        <tr>
+                                            <th scope="col" className="px-6 py-4">#</th>
+                                            <th scope="col" className="px-6 py-4">PHOTO</th>
+                                            <th scope="col" className="px-6 py-4">NAME</th>
+                                            <th scope="col" className="px-6 py-4">ROLE</th>
+                                            <th scope="col" className="px-6 py-4">ROLE</th>
+                                            <th scope="col" className="px-6 py-4">ROLE</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {
+                                            users.map((user, idx) => <tr
+                                                key={user._id}
+                                                className="border-b transition duration-300 ease-in-out hover:bg-neutral-100 dark:border-neutral-500 dark:hover:bg-neutral-600">
+                                                <td className="whitespace-nowrap px-6 py-4 font-medium">{idx + 1}</td>
+                                                <td className="whitespace-nowrap px-6 py-4">
+                                                    <img src={user.photoUrl} className='h-[35px] w-[35px]' alt="" />
+                                                </td>
+                                                <td className="whitespace-nowrap px-6 py-4">{user.name}</td>
+                                                <td className="whitespace-nowrap px-6 py-4">{user.role}</td>
+                                                <td className="whitespace-nowrap px-6 py-4">
+                                                    <span className='inline-flex items-center gap-2 cursor-pointer bg-green-500 py-1 rounded-md px-2 text-white'>Update <GrUpdate className='text-white' /></span>
+                                                </td>
+                                                <td className="whitespace-nowrap px-6 py-4">
+                                                <span className='inline-flex items-center gap-2 cursor-pointer bg-red-600 py-1 rounded-md px-2 text-white'>Delete <FcDeleteDatabase /></span>
+                                                </td>
+                                            </tr>)
+                                        }
+
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
