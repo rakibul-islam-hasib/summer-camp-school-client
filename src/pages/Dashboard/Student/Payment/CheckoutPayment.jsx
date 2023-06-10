b957d97fdb1e3972ab6a92e4e916bb1163ac6da7
Hasib Hossain <boraborhasib@gmail.com>
2023-06-10 22:41:08 +0600

    Payment Page updated

--- src/pages/Dashboard/Student/Payment/CheckoutPayment.jsx
+++ src/pages/Dashboard/Student/Payment/CheckoutPayment.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import useAxiosSecure from '../../../../hooks/useAxiosSecure';
+import { useUser } from '../../../../hooks/useUser';
 const CheckoutPayment = ({ price }) => {
     const stripe = useStripe();
     const elements = useElements();
+    const axiosSecure = useAxiosSecure();
+    const { currentUser, isLoading } = useUser();
+    const [clientSecret, setClientSecret] = useState('');
+    const [message, setMessage] = useState('');
+    const [classes, setClasses] = useState([]);
+    useEffect(() => {
+        axiosSecure.get(`/cart/${currentUser?.email}`)
+            .then((res) => {
+                // SET CLASSES ID IN STATE
+                const classesId = res.data.map(item => item._id);
+                setClasses(classesId)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }, [])
+    useEffect(() => {
+        axiosSecure.post('/create-payment-intent', { price: price })
+            .then(res => {
+                setClientSecret(res.data.clientSecret)
+                console.log(res.data)
 
+            })
+    }, [])
     const handleSubmit = async (event) => {
+        setMessage('')
         event.preventDefault();
         if (!stripe || !elements) {
             return;
@@ -19,8 +45,61 @@ const CheckoutPayment = ({ price }) => {
         });
         if (error) {
             console.log('[error]', error);
+            setMessage(error.message)
         } else {
             console.log('[PaymentMethod]', paymentMethod);
+        }
+
+        const { paymentIntent, error: confirmError } = await stripe.confirmCardPayment(
+            clientSecret,
+            {
+                payment_method: {
+                    card: card,
+                    billing_details: {
+                        name: currentUser.name || 'Unknown',
+                        email: currentUser.email || 'Anonymous',
+                    },
+                },
+            },
+        );
+        if (confirmError) {
+            console.log('[error]', confirmError);
+            setMessage(confirmError.message)
+        }
+        else {
+            console.log('[PaymentMethod]', paymentIntent);
+
+            // PAYMENT LOGIC HERE WHEN PAYMENT IS SUCCESSFUL
+            if (paymentIntent.status === 'succeeded') {
+                const transactionId = paymentIntent.id;
+                const paymentMethod = paymentIntent.payment_method;
+                const amount = paymentIntent.amount / 100;
+                const currency = paymentIntent.currency;
+                const paymentStatus = paymentIntent.status;
+                const userName = currentUser.name;
+                const userEmail = currentUser.email;
+                const classesId = classes;
+                const data = {
+                    transactionId,
+                    paymentMethod,
+                    amount,
+                    currency,
+                    paymentStatus,
+                    userName,
+                    userEmail,
+                    classesId
+                }
+                axiosSecure.post('/payment-info', data)
+                .then(res => { 
+                    console.log(res.data)
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+
+            }
+
+
         }
     }
 
@@ -42,9 +121,10 @@ const CheckoutPayment = ({ price }) => {
                     },
                 }}
             />
-            <button type="submit" disabled={!stripe}>
+            <button type="submit" disabled={!stripe || !clientSecret || isLoading}>
                 Pay
             </button>
+            {message && <p className="text-red-500">{message}</p>}
         </form>
     );
 };
